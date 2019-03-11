13911609117a13c4bf2109f3f45ffe3e89e83752
Sam Lichlyter <slichlyter12@me.com>
2019-03-11 16:06:44 -0700

    connected to db and started on main logic

---
+++ logic.php
@@ -0,0 +1,24 @@
+<?php
+	
+	include_once('dbconnect.php');
+	
+	function getIndeces($mysqli, $city, $stateAbbreviation) {
+		if (!($res = $mysqli->query("select p.p_name,count(j.jid),avg(j.j_salary_max),avg(j.j_salary_min) ,max(i.i_quality_of_life)  
+	        from cm_job j left join cm_company c ON j.j_company = c.cid 
+	    	left join cm_profession p ON j.j_profession = p.pid
+	    	left join cm_location l ON j.j_location = l.lid 
+	    	left join cm_index i on l.lid = i.i_location  
+	    	where l.l_state = '$stateAbbreviation' and l.l_city = '$city'
+	    	group by l.l_state,p.p_name"))) {
+				echo "Failed Select";
+		}
+	
+		$data = [];
+		while ($row = $res->fetch_assoc()) {
+			array_push($data, $row);	
+		}
+		
+		return $data;
+	}
+	
+?>
